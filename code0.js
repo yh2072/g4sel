dd7c600f86ea574cebbaafa4c5c1d96475dbda22
yh2072 <146281248+yh2072@users.noreply.github.com>
2024-03-07 15:21:17 -0500

    Add files via upload

---
+++ code0.js
@@ -0,0 +1,35 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1= [];
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2= [];
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+{
+
+
+let isConditionTrue_0 = false;
+isConditionTrue_0 = false;
+isConditionTrue_0 = gdjs.evtTools.runtimeScene.sceneJustBegins(runtimeScene);
+if (isConditionTrue_0) {
+{gdjs.playerAuthentication.displayAuthenticationBanner(runtimeScene);
+}}
+
+}
+
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects1.length = 0;
+gdjs.Untitled_32sceneCode.GDNewSpriteObjects2.length = 0;
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
